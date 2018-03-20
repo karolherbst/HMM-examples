bffd8efcf8134ec1215d25d6c7c684a6aea308f3
Karol Herbst <kherbst@redhat.com>
2018-03-20 22:08:42 +0100

    fma_struct: calculate correct results before launching the kernel

--- fma_struct.cpp
+++ fma_struct.cpp
@@ -14,22 +14,18 @@ int main() {
 	cl::Kernel kernel = initCL("fma_struct.cl", queue);
 	cl_int ret;
 
-	constexpr uint64_t size = 100;
+	constexpr uint64_t size = 1000;
 	uint64_t res[size];
 	struct FMAData data[size];
+	uint64_t correct_results[size];
 
 	for (int i = 0; i < size; ++i) {
 		data[i].a = rand() % 0x200 + 1;
 		data[i].b = rand() % 0x200 + 1;
 		data[i].c = rand() % 0x200 + 1;
 		res[i] = 0;
+		correct_results[i] = data[i].a * data[i].b + data[i].c;
 	}
-	data[0].a = 0x10;
-	data[0].b = 0x200;
-	data[0].c = 0x30;
-	data[1].a = 0x20;
-	data[1].b = 0x300;
-	data[1].c = 0x40;
 
 	ret = kernel.setArg(0, &res);
 	ret |= kernel.setArg(1, &data);
@@ -42,9 +38,9 @@ int main() {
 	queue.finish();
 
 	for (int i = 0; i < size; ++i) {
-		uint64_t result = data[i].a * data[i].b + data[i].c;
+		uint64_t result = correct_results[i];
 		if (res[i] != result) {
-			std::cerr << "Wrong result " << i << ": " << res << " != " << result << std::endl;
+			std::cerr << "Wrong result " << i << ": " << res[i] << " != " << result << std::endl;
 			return EXIT_FAILURE;
 		} else
 			std::cout << "Result is correct! " << data[i].a << " * " << data[i].b << " + " << data[i].c << " = " << result << std::endl;
