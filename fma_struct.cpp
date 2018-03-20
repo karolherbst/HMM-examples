a1a41c93742e164de55e398e00b1e54fddcf9d81
Karol Herbst <kherbst@redhat.com>
2018-03-20 22:04:09 +0100

    fma_struct: loop with many results

--- fma_struct.cpp
+++ fma_struct.cpp
@@ -1,16 +1,29 @@
+#include <time.h>
+
+#include <cstdlib>
+
 #include <iostream>
 
 #include "base.h"
 #include "fma_struct.h"
 
 int main() {
+	srand(time(nullptr));
+
 	cl::CommandQueue queue;
 	cl::Kernel kernel = initCL("fma_struct.cl", queue);
 	cl_int ret;
 
-	constexpr uint64_t size = 2;
+	constexpr uint64_t size = 100;
 	uint64_t res[size];
 	struct FMAData data[size];
+
+	for (int i = 0; i < size; ++i) {
+		data[i].a = rand() % 0x200 + 1;
+		data[i].b = rand() % 0x200 + 1;
+		data[i].c = rand() % 0x200 + 1;
+		res[i] = 0;
+	}
 	data[0].a = 0x10;
 	data[0].b = 0x200;
 	data[0].c = 0x30;
@@ -25,7 +38,7 @@ int main() {
 		std::cerr << "Failed to set args" << std::endl;
 	}
 
-	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(2), cl::NDRange(1), nullptr, nullptr);
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size), cl::NDRange(1), nullptr, nullptr);
 	queue.finish();
 
 	for (int i = 0; i < size; ++i) {
@@ -33,8 +46,8 @@ int main() {
 		if (res[i] != result) {
 			std::cerr << "Wrong result " << i << ": " << res << " != " << result << std::endl;
 			return EXIT_FAILURE;
-		}
+		} else
+			std::cout << "Result is correct! " << data[i].a << " * " << data[i].b << " + " << data[i].c << " = " << result << std::endl;
         }
-	std::cout << "Result is correct!" << std::endl;
 	return EXIT_SUCCESS;
 }
