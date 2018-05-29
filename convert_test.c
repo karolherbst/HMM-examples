0fc7fad52afe27139c4a42fcc885657f62272bf4
Karol Herbst <kherbst@redhat.com>
2018-05-29 17:31:38 +0200

    add test for convert constant folding

---
+++ convert_test.c
@@ -0,0 +1,102 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "base.h"
+
+int main() {
+	srand(time(NULL));
+
+	cl_context context;
+	cl_command_queue queue;
+	cl_kernel kernel;
+	cl_device_svm_capabilities svm;
+	cl_int ret;
+
+	ret = initCL("convert_test.cl", &context, &kernel, &queue, &svm);
+	if (ret)
+		return ret;
+
+	const uint32_t size32 = 20;
+	const uint32_t size64 = 1;
+	const uint64_t byteSize32 = size32 * sizeof(uint32_t);
+	const uint64_t byteSize64 = size64 * sizeof(uint64_t);
+
+	uint32_t *res32 = malloc(byteSize32);
+	uint64_t *res64 = malloc(byteSize64);
+
+	uint32_t *result32 = malloc(byteSize32);
+	uint64_t *result64 = malloc(byteSize64);
+
+	// double -> int: rte, rtz, rtp, rtn
+	result32[0]  =  1;
+	result32[1]  =  0;
+	result32[2]  =  1;
+	result32[3]  =  0;
+	// close nextafter(1.0, 0) double -> float: rte, rtz, rtp, rtn
+	result32[4]  =  0x3f7fffff;
+	result32[5]  =  0x3f7fffff;
+	result32[6]  =  0x3f800000;
+	result32[7]  =  0x3f7fffff;
+	// -double -> int: rte, rtz, rtp, rtn
+	result32[8]  = -1;
+	result32[9]  =  0;
+	result32[10] =  0;
+	result32[11] = -1;
+	// +-0.5 +-1.5 -> int: rte
+	result32[12] =  1;
+	result32[13] =  2;
+	result32[14] = -1;
+	result32[15] = -2;
+	// close 1.0 double -> float: rte, rtz, rtp, rtn
+	result32[16]  =  0x3f800000;
+	result32[17]  =  0x3f7fffff;
+	result32[18]  =  0x3f800000;
+	result32[19]  =  0x3f7fffff;
+
+	result64[0] = 0;
+
+	cl_mem outBuffer32 = clCreateBuffer(context, CL_MEM_WRITE_ONLY, byteSize32, NULL, &ret);
+	cl_mem outBuffer64 = clCreateBuffer(context, CL_MEM_WRITE_ONLY, byteSize64, NULL, &ret);
+
+	ret  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &outBuffer32);
+	ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &outBuffer64);
+
+	if (ret) {
+		printf("Failed to set args\n");
+		return ret;
+	}
+
+	const size_t global_size[3] = { 1, 0, 0 };
+	const size_t local_size[3] = { 1, 0, 0 };
+	ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_size, local_size, 0, NULL, NULL);
+	if (ret) {
+		printf("Failed to enqueue kernel\n");
+		return ret;
+	}
+
+	clEnqueueReadBuffer(queue, outBuffer32, 0, 0, byteSize32, res32, 0, NULL, NULL);
+	clEnqueueReadBuffer(queue, outBuffer64, 0, 0, byteSize64, res64, 0, NULL, NULL);
+	clFinish(queue);
+
+	bool error = false;
+	for (int i = 0; i < size32; ++i) {
+		if (result32[i] != res32[i]) {
+			error = true;
+			printf("Wrong result32 %i: %x != %x\n", i, result32[i], res32[i]);
+		}
+	}
+
+	for (int i = 0; i < size64; ++i) {
+		if (result64[i] != res64[i]) {
+			error = true;
+			printf("Wrong result64 %i: %lx != %lx\n", i, result64[i], res64[i]);
+		}
+	}
+
+	if (!error)
+		printf("No Errors!\n");
+
+	return EXIT_SUCCESS;
+}
---
+++ convert_test.cl
@@ -0,0 +1,37 @@
+kernel void test(global uint* out32, global ulong* out64)
+{
+	global float *fout = (global float*)out32;
+	global double *dout = (global double*)out64;
+
+	// double to int
+	out32[0] = convert_int_rte(0x0.ffffffffP0);
+	out32[1] = convert_int_rtz(0x0.ffffffffP0);
+	out32[2] = convert_int_rtp(0x0.ffffffffP0);
+	out32[3] = convert_int_rtn(0x0.ffffffffP0);
+
+	// double to float
+	fout[4] = convert_float_rte(0x0.ffffff2P0);
+	fout[5] = convert_float_rtz(0x0.ffffff2P0);
+	fout[6] = convert_float_rtp(0x0.ffffff2P0);
+	fout[7] = convert_float_rtn(0x0.ffffff2P0);
+
+	// -double to int
+	out32[8] = convert_int_rte(-0x0.ffffffffP0);
+	out32[9] = convert_int_rtz(-0x0.ffffffffP0);
+	out32[10] = convert_int_rtp(-0x0.ffffffffP0);
+	out32[11] = convert_int_rtn(-0x0.ffffffffP0);
+
+	// +-0.5 +-1.5 double to int rte
+	out32[12] = convert_int_rte( 0x0.8P0);
+	out32[13] = convert_int_rte( 0x1.8P0);
+	out32[14] = convert_int_rte(-0x0.8P0);
+	out32[15] = convert_int_rte(-0x1.8P0);
+
+	// double to float
+	fout[16] = convert_float_rte(0x0.fffffffffffff7P0);
+	fout[17] = convert_float_rtz(0x0.fffffffffffff7P0);
+	fout[18] = convert_float_rtp(0x0.fffffffffffff7P0);
+	fout[19] = convert_float_rtn(0x0.fffffffffffff7P0);
+
+	out64[0] = 0;
+}
